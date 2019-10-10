fab21770aeab23a8771080054f9c89d89cb8a07d
kamalogudah <mcpaul2058@gmail.com>
2019-10-10 22:37:14 +0300

    Query for Returning links

--- lib/community_graphql_web/schema.ex
+++ lib/community_graphql_web/schema.ex
@@ -3,6 +3,13 @@ defmodule CommunityGraphqlWeb.Schema do
   
   alias CommunityGraphqlWeb.NewsResolver
   
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field :description, non_null(:string)
+  end
+  
   query do
+    field :all_links, non_null(list_of(non_null(:link)))
   end
 end
